9245133fdce72bfbd93e802559d75925701511f4
Amuza MUGISHA <86699509+nisloy@users.noreply.github.com>
2026-05-08 10:10:30 +0200

    Add balanced balanced checking functionality
    Implemented the isBalanced method to check for balanced parentheses and added a matching pair helper function.

--- Balanced.java
+++ Balanced.java
@@ -3,14 +3,30 @@ public class Balanced{
     public static void main(String[] args) {
         String[] str = {"({[]})","({[})", "((()))", "({)}"};
         for(String s: str)
-         System.out.println(isBalanced(s));
-        
+            System.out.println(isBalanced(s));
     }
     public static boolean isBalanced(String s){
-        //
-        
-        return false;
-}
-        
-    
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if( ch=='{' || ch=='[' || ch=='(' ) {
+                stack.push(ch);
+            }
+            else if (ch == ')' || ch == ']' || ch == '}') {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                char lastChar=stack.pop();
+                if (!isMatchingPair(lastChar, ch)) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static boolean isMatchingPair(char opener,char closer){
+        boolean result = (opener=='(' && closer==')') || (opener=='{' && closer=='}') || (opener=='[' && closer==']');
+        return result;
+    }
+
+
 }
